d6b84590bf79a9e5d49e98bca56f864097b36a6d
VilMano <mano.gsat@hotmail.com>
2024-07-15 23:23:32 +0100

    Model setup

---
+++ api/Models/Movement.cs
@@ -0,0 +1,9 @@
+public class Movement{
+    public int Id { get; set; }
+    public float Value { get; set; } = 0;
+    public string Description { get; set; } = string.Empty;
+    public float UserShare { get; set; } = 0;
+
+    public User User { get; set; } = new User();
+    public string UserId { get; set; } = string.Empty;
+}
